584b1af3d5dc444d170db3598419f2378e27ffd4
Serzh <you@example.com>
2021-11-21 18:40:58 +0200

    complit dz

--- src/index.js
+++ src/index.js
@@ -10,9 +10,20 @@ const App = () => {
             id: 'parent'
         },
         React.createElement('div', {}, 'hello world', [
-            React.createElement('h1', {}, 'hello world'),
+            React.createElement('h1', {id: 'time'}, ),
             React.createElement('h2', {}, 'hello world'),
             React.createElement(Todo, { title: 'learn', date: '01.01.2011' }),
         ]))
 }
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
+
+function time() {
+    console.log(document.getElementById('timeClass'))
+    let date = new Date().toLocaleTimeString();
+    const timeElement = (
+        React.createElement('span', {},'time is '+ date )
+        );
+    // ReactDOM.render(React.createElement(App), document.getElementsByClassName('timeClass'));
+    ReactDOM.render(timeElement, document.getElementById('time'))
+}
+setInterval( time,1000)
